693fdb4d94f3b461af44b0ab397b6450da8aead2
sia1010 <sia_21001570@utp.edu.my>
2025-08-26 23:17:40 +0800

    small change to makedir

--- generateData/makedirectory.py
+++ generateData/makedirectory.py
@@ -1,8 +1,11 @@
  # Ensure folders exists
 import os
-os.makedirs(os.path.dirname("data/"), exist_ok=True)
-os.makedirs(os.path.dirname("data/src_shopee/"), exist_ok=True)
-os.makedirs(os.path.dirname("data/src_lazada/"), exist_ok=True)
-os.makedirs(os.path.dirname("data/src_tiktok/"), exist_ok=True)
-os.makedirs(os.path.dirname("data/src_pos/"), exist_ok=True)
-os.makedirs(os.path.dirname("data/wh/"), exist_ok=True)
+def run():
+    os.makedirs(os.path.dirname("data/"), exist_ok=True)
+    os.makedirs(os.path.dirname("data/src_shopee/"), exist_ok=True)
+    os.makedirs(os.path.dirname("data/src_lazada/"), exist_ok=True)
+    os.makedirs(os.path.dirname("data/src_tiktok/"), exist_ok=True)
+    os.makedirs(os.path.dirname("data/src_pos/"), exist_ok=True)
+    os.makedirs(os.path.dirname("data/wh/"), exist_ok=True)
+
+run()
